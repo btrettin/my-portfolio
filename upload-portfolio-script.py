ee418c838dc96cdb68d06e86de7e06f9ee3c86e4
btrettin <batrettin@gmail.com>
2019-06-05 00:00:39 -0500

    added sns to my deploy lambda

--- upload-portfolio-script.py
+++ upload-portfolio-script.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 from botocore.client import Config
 import StringIO
@@ -6,21 +5,29 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-2:655915645761:deployPortfolio')
 
-    portfolio_bucket = s3.Bucket('portfolio.bentrettin.com')
-    build_bucket = s3.Bucket('portfoliobuild.bentrettin.com')
+    try:
+        s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('buildmyportfolio.zip',portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.bentrettin.com')
+        build_bucket = s3.Bucket('portfoliobuild.bentrettin.com')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('buildmyportfolio.zip',portfolio_zip)
 
-    print 'job done!'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job done!'
+        topic.publish(Subject='Portfolio Deployed', Message='Portfolio Deployed Succesfully')
+    except:
+        topic.publish(Subject='Portfolio Deploye Failed', Message='The Portfolio was not deployed succesfully')
+        raise
 
     return 'Hello from Lambda'
