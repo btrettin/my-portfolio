5a74f0745e416abacb7a345484dbf81f777e1287
Ben Trettin <batrettin@gmail.com>
2019-06-09 21:39:11 -0500

    added devops

--- src/actions/create-redux-action/create-redux-action.js
+++
@@ -1,2 +0,0 @@
-export const createReduxAction = actionType => data => ({ type: actionType, data });
-export default createReduxAction;
--- src/actions/login-modal/login-modal-actions.js
+++
@@ -1,5 +0,0 @@
-import Actions from '../../reducers/actions';
-import createReduxAction from '../create-redux-action';
-
-export const toggleLoginModal = createReduxAction(Actions.login.toggleLoginModal);
-export const swapModal = createReduxAction(Actions.login.swapModal);
