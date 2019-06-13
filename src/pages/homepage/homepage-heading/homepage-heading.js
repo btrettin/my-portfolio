1c5e35a76d028f79da3d73ea6f268d2b76cebee7
btrettin <batrettin@gmail.com>
2019-06-12 23:03:30 -0500

    changed header

--- src/pages/homepage/homepage-heading/homepage-heading.js
+++ src/pages/homepage/homepage-heading/homepage-heading.js
@@ -12,15 +12,28 @@ const ButtonDiv = styled.div`
 `;
 
 const HomePageHeading = props => (
-  <Grid container sm={12} spacing={24} className={styles.homepageHeadingContainer}>
-    <Grid container sm={12} spacing={24} className={styles.headingShadowContainer}>
+  <Grid
+    container
+    sm={12}
+    spacing={24}
+    className={styles.homepageHeadingContainer}
+  >
+    <Grid
+      container
+      sm={12}
+      spacing={24}
+      className={styles.headingShadowContainer}
+    >
       <Grid item sm={12} className={styles.headingHeader}>
         <h1 className={styles.header}>Lawn Nanny</h1>
       </Grid>
       <Grid item sm={12} className={styles.buttonContainer}>
         <ButtonDiv className={styles.buttonDiv}>
-          <Button className={styles.requestButton} onClick={() => props.history.push('/pipeline')}>
-            Make a Request
+          <Button
+            className={styles.requestButton}
+            onClick={() => props.history.push('/pipeline')}
+          >
+            Make a Request if you want to loser
           </Button>
         </ButtonDiv>
       </Grid>
@@ -29,13 +42,13 @@ const HomePageHeading = props => (
 );
 
 HomePageHeading.contextTypes = {
-  history: PropTypes.func,
+  history: PropTypes.func
 };
 HomePageHeading.propTypes = {
-  history: PropTypes.func,
+  history: PropTypes.func
 };
 
 HomePageHeading.defaultProps = {
-  history: PropTypes.func,
+  history: PropTypes.func
 };
 export default HomePageHeading;
