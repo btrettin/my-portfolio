f29f6f8d38fd020133008373ae2586750f85070d
btrettin <batrettin@gmail.com>
2019-06-14 12:37:50 -0500

    retittled homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -4,7 +4,7 @@ import styles from './homepage.module.css';
 
 const HomePage = () => (
   <Grid container spacing={24} className={styles.homepageContainer}>
-    <div>hello,world this is ben trettin</div>
+    <div>My Portoflio</div>
   </Grid>
 );
 export default HomePage;
