32826b7b3c725345a45c5db26f578d075053038a
btrettin <batrettin@gmail.com>
2019-06-14 01:07:37 -0500

    changed menu

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton';
-import SvgIcon from '@material-ui/core/SvgIcon';
+import Button from '@material-ui/core/Button';
 import styles from './navbar.module.css';
-
-const HomeIcon = props => (
-  <SvgIcon {...props} className={styles.homeIcon}>
-    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-  </SvgIcon>
-);
 
 export const Navbar = () => (
   <AppBar position="static" className={styles.Menu}>
     <div className={styles.leftMenuDiv}>
       <Link to="/HomePage">
-        <IconButton color="inherit" aria-label="Menu">
-          <HomeIcon />
-        </IconButton>
+        <Button className={styles.button}>Home</Button>
       </Link>
+      <Button className={styles.button}>About Me</Button>
+      <Button className={styles.button}>Projects</Button>
+      <Button className={styles.button}>Contact</Button>
     </div>
+    <div className={styles.rightMenuDiv} />
   </AppBar>
 );
 
