49c412cb09aaa4d47af77ba55f6b0551b054e45e
btrettin <batrettin@gmail.com>
2019-06-17 16:01:18 -0500

    added gituhb/linkedin links

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -4,7 +4,17 @@ import styles from './homepage.module.css';
 
 const HomePage = () => (
   <Grid container spacing={24} className={styles.homepageContainer}>
-    <div>My Portoflio</div>
+    <div className={styles.headerDiv}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.header}> About Me </h1>
+        <div className={styles.content}>
+          My name is Ben and I am a full stack devlooper. I am passionate about learning new
+          technologies and software. Recently Ive became very interested in AWS. Actually, I created
+          this website as a place to experiment with the many features of AWS and continue to grow
+          as a developer.
+        </div>
+      </div>
+    </div>
   </Grid>
 );
 export default HomePage;
